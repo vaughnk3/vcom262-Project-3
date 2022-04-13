7e53e7171ad59b4bdcbeb07099114b6af3c97d12
Mrrego01 <regom2@mailbox.winthrop.edu>
2022-04-13 02:44:59 -0400

    Dictionary is (mostly) finished (except for all of the rest of it)

--- scripts/dicScripts.js
+++ scripts/dicScripts.js
@@ -25,3 +25,80 @@ af_content.forEach(af_grid_item => {
         }
     });
 });
+
+const gl_content = document.querySelectorAll(".gl_grid_item");
+
+var gl_contentARR = Array.prototype.slice.call(gl_content);
+
+gl_content.forEach(gl_grid_item =>{
+
+gl_grid_item.addEventListener('click', function glClick(event){
+
+
+    var gl_place = document.getElementById("dir_gl");
+    var gl_check = gl_contentARR.indexOf(gl_grid_item);
+
+    var gl_def = ["The combination of gears used between the front chainring and the rear gear cluster", "The lowest gear ratio on a multi-speed bicycle, smallest chainring in front and largest in back", " A mountain bike which only has front suspension", "Taking the lead position out of the starting gate going into the first turn, used in BMX racing", "A sudden acceleration, often at the start of a sprint", "The final burst of speed in a sprint",
+                  "French for Red Lantern, as found at the end of a railway train. Used to describe the last-placed rider in a race", "A team-oriented move in which one rider begins a sprint to give a head start to a teammate riding in his draft"]
+
+    for (var i = 0; i <= gl_contentARR.length; i++)
+    {
+        if (i == gl_check)
+        {
+            gl_place.innerHTML = gl_def[i];
+        }
+    }
+})
+});
+
+const ms_content = document.querySelectorAll(".ms_grid_item");
+
+var ms_contentARR = Array.prototype.slice.call(ms_content);
+
+ms_content.forEach(ms_grid_item =>{
+
+ms_grid_item.addEventListener('click', function msClick(event){
+
+    var ms_place = document.getElementById("dir_ms");
+    var ms_check = ms_contentARR.indexOf(ms_grid_item);
+
+    var ms_def = ["Used to describe a heat of a BMX race", "A bag filled with food, water and energy drinks distributed to riders in a feed zone", "Cycling’s term for a rookie at the professional level", "The main group of riders during a road race", "A mid-race contest in which riders sprint for points, cash or prizes during a criterium, sometimes used to animate a race.", " A short individual time trial at the beginning of a stage race; used to assign a leader’s jersey for the first stage",
+                  "To take a turn at the front of a paceline and block the wind for other riders in the pack", "When a rider moves to the front of a paceline and takes his turn blocking the wind at the front", "When a rider moves to one side of a paceline to allow another rider to pull through.", "A vehicle that follows a race and picks up riders who are unable to finish", "A narrow off-road trail which is only wide enough for bikes to ride in a single-file formation", 
+                  "When one rider refuses to take a pull and break the wind for a group in which he’s riding", "The pocket of air created by a moving rider, just as in automobile or motorcycle racing", "A mountain bike that has both front and rear suspension", "A French term for cycling’s equivalent of a trainer. A soigneur usually looks after the physical health of a rider and gives massages", "The sudden burst of speed before the race’s finish or an intermediate sprint point",
+                  "The space between the Black Line and The Sprinter’s Line on the velodrome. A rider leading in this zone cannot be passed on the inside", "A 5-centimeter-wide line 90-centimeters above the Black Line", "A multi-day event which combines several races. The rider with the lowest cumulative time is declared the winner", "An amateur rider who gets an opportunity to compete with a professional team during the season in order to gain experience at the pro level"]
+
+    for (var i = 0; i <= ms_contentARR.length; i++)
+    {
+        if (i == ms_check)
+        {
+            ms_place.innerHTML = ms_def[i]
+        }
+    }
+})
+});
+
+const tz_content = document.querySelectorAll(".tz_grid_item");
+
+var tz_contentARR = Array.prototype.slice.call(tz_content);
+
+tz_content.forEach(tz_grid_item =>{
+
+    tz_grid_item.addEventListener('click', function tzClick(event){
+
+        var tz_place = document.getElementById("dir_tz");
+        var tz_check = tz_contentARR.indexOf(tz_grid_item);
+
+        var tz_def = ["A section on a mountain bike course where riders can exchange parts or receive mechanical assistance from others", "When a rider attacks the main pack and creates a solo breakaway", "An individual or team race against the clock", "A technique used in track racing in which competitors come to a complete standstill in order to force their opponent to take the lead",
+                      "An oval, banked cycling track used for track racing", "A derogatory term used to describe someone who rides in a pack without taking a pull at the front and blocking the wind"]
+
+        for (var i = 0; i <= tz_contentARR.length; i++)
+        {
+            if (i == tz_check)
+            {
+                tz_place.innerHTML = tz_def[i];
+            }
+        }
+
+    });
+
+});
