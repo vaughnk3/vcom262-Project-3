f3e7183f9f7d600115a2b0dd8dd5dc4d5c0fba70
Mrrego01 <regom2@mailbox.winthrop.edu>
2022-04-24 04:52:46 -0400

    I stared deep into the crystal ball of wizardry and came out the other side a madman

--- scripts/cart.js
+++ scripts/cart.js
@@ -2,26 +2,25 @@ var images = JSON.parse(sessionStorage.getItem("img"))
 var names = JSON.parse(sessionStorage.getItem("nam"))
 var costs = JSON.parse(sessionStorage.getItem("cos"))
 var places = parseInt(sessionStorage.getItem("place"))
+var once = 0;
 
-function shoppingcnt (num)
-{
+
+function shoppingcnt(num) {
     document.getElementById("carts").innerText = num;
 }
 
-if(images)
-{
+if (images) {
     var cartnum = images.length;
     document.onload = shoppingcnt(cartnum);
 }
-else{
+else {
     cartnum = 0;
     document.getElementById("carts").style.display = "none";
 }
 
 
 
-function newitem (img,nom,cos)
-{
+function newitem(img, nom, cos) {
     //making the tag
     var tag = document.createElement("div");
     tag.classList.add("item");
@@ -48,48 +47,65 @@ function newitem (img,nom,cos)
     document.getElementById("item_List").insertBefore(tag, document.getElementById("item_List").childNodes.item(1));
 
 
-    var removal = document.querySelectorAll(".remove")
+    var removal = document.getElementsByClassName("remove")
     var rem_Tag = document.querySelectorAll(".item")
 
     var removalARR = Array.prototype.slice.call(removal);
+    var scoutARR = images.length -1;
+    console.log(scoutARR)
 
-removal.forEach(removes => {
-
-    removes.addEventListener('click', function update(event){
-        if(!event.detail || event.detail == 1)
-        {
-        var remove_check = removalARR.indexOf(removes);
-        var mover = cartnum;
-        for (i = mover; i >= mover - remove_check; i--)
-        {
-            if (i == mover - remove_check)
-            {
-                i = i - 1;
-                images.splice(i,1);
-                names.splice(i,1);
-                costs.splice(i,1);
+    removalARR.forEach(removes => {
+        removes.addEventListener('click', function update(event) {
+            if (event.timeStamp == once) {
+                //nothing
             }
-        }
-        rem_Tag.item(remove_check).remove();
-        console.log(places)
-        places = places - 1;
+            else {
 
-        // sessionStorage.setItem("img",JSON.stringify(images));
-        // sessionStorage.setItem("nam",JSON.stringify(names));
-        // sessionStorage.setItem("cos",JSON.stringify(costs));
-        // sessionStorage.setItem("place",places);
-    }
+
+                console.log(removalARR.length)
+                console.log(scoutARR)
+                if (scoutARR >= removalARR.length)
+                {
+                    console.log("pringus")
+                    var remove_check = removalARR.length - 1;
+                }
+                else
+                {
+                    console.log("bringus")
+                    var remove_check = scoutARR;
+                }
+                console.log("remove_check:" + remove_check);
+                var mover = cartnum - 1;
+                for (i = mover; i >= remove_check; i--) {
+                    if (i == remove_check) {
+                        images.splice(i, 1);
+                        names.splice(i, 1);
+                        costs.splice(i, 1);
+                        scoutARR = scoutARR - 1;
+                        console.log(scoutARR);
+                    }
+                }
+                rem_Tag.item(0).remove();
+                places = places - 1;
+
+                sessionStorage.setItem("img", JSON.stringify(images));
+                sessionStorage.setItem("nam", JSON.stringify(names));
+                sessionStorage.setItem("cos", JSON.stringify(costs));
+                sessionStorage.setItem("place", places);
+                cartnum = images.length;
+                shoppingcnt(cartnum);
+                once = event.timeStamp;
+                location.reload();
+            }
+        });
     });
-});
 }
 var sum = 0;
 
-function funny(){
-for (var i = 0; i < cartnum; i++)
-{
-    console.log("here")
-    newitem(images[i],names[i],costs[i]);
-    sum += parseFloat(costs[i]);
-}
-document.getElementById("money").innerHTML = sum.toFixed(2);
+function funny() {
+    for (var i = 0; i < cartnum; i++) {
+        newitem(images[i], names[i], costs[i]);
+        sum += parseFloat(costs[i]);
+    }
+    document.getElementById("money").innerHTML = sum.toFixed(2);
 }
